4ee0675c83bf2c4de46a9b1a87f3f5c501d5208d
hessennasser <92742551+hessennasser@users.noreply.github.com>
2022-11-12 11:57:38 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');    
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
     });
 
-    $(window).on('scroll load',function(){
+    $(window).on('scroll load', function () {
 
-        $('#menu').removeClass('fa-times');    
+        $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
 
-        if($(window).scrollTop() > 0){
+        if ($(window).scrollTop() > 0) {
             $('.top').show();
-        }else{
+        } else {
             $('.top').hide();
         }
 
@@ -20,14 +20,14 @@ $(document).ready(function(){
 
     // smooth scrolling 
 
-    $('a[href*="#"]').on('click',function(e){
-       e.preventDefault(); 
-       $('html, body').animate({
-        scrollTop : $($(this).attr('href')).offset().top,
-       },
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
             500,
             'linear'
-       );
+        );
     });
 
 });
